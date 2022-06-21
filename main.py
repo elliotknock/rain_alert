e1fd395048da2a444709747a13c74fdf36b2801a
Elliot <EKnock211@gmail.com>
2022-06-21 10:13:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "69f04e4613056b159c2761a9d9e664d2"
+
+weather_params = {
+    "lat": 4.394673,
+    "lon": 18.558189,
+    "appid": "69f04e4613056b159c2761a9d9e664d2",
+    "exclude": "current,minutely,daily",
+}
+
+will_rain = False
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
